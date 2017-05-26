a076599810a24d26cff624acaa8ac29ba6a94e15
Jon Huh <oneandwholly@gmail.com>
2017-05-26 16:54:00 +0900

    make middleware to wait for promise to resolve

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,13 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
 
-    next(action);
+    action.payload
+      .then(response => {
+        const newAction = { ...action, payload: response.data };
+        dispatch(newAction);
+      });
   };
 }
