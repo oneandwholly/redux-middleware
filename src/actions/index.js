08eb833f8948c0a46705ca19f07a4af410cd7d60
Jon Huh <oneandwholly@gmail.com>
2017-05-26 16:12:54 +0900

    make get request with axios and add it to the action payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,6 @@ export function fetchUsers() {
 
   return {
     type: FETCH_USERS,
-    payload: [
-      { name: 'Jane' },
-      { name: 'Alex' },
-      { name: 'Jim' }
-    ]
+    payload: request
   };
 }
